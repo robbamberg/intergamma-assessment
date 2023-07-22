4e5235eb724c3f34fe592d17927381b15ca82840
robbamberg <rbamberg@maqqie.nl>
2023-07-22 19:52:16 +0200

    Init project files

---
+++ src/main/kotlin/com/robbamberg/intergammaassessment/IntergammaAssessmentApplication.kt
@@ -0,0 +1,11 @@
+package com.robbamberg.intergammaassessment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IntergammaAssessmentApplication
+
+fun main(args: Array<String>) {
+    runApplication<IntergammaAssessmentApplication>(*args)
+}
